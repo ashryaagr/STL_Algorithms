54402d16a1e30b7964a1884a46d5af947bea052b
kishansinha181 <kishan.sinha18@gmail.com>
2021-10-07 14:06:46 +0530

    added a file
    new PR

---
+++ addTwoNumbersLinkedList.cpp
@@ -0,0 +1,40 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        int sum = 0;
+        int carry = 0;
+        ListNode *dummy = new ListNode(-1);
+        ListNode*temp= dummy;
+        while(l1 || l2 || carry){
+
+            sum = 0;
+            if(l1){
+                sum+= l1->val;
+                l1 = l1->next;
+                
+            }
+            if(l2){
+                sum+= l2->val;
+                l2 = l2->next;
+            }
+            
+            sum+= carry;
+            carry = sum/10;
+            temp->next = new ListNode(sum%10);
+            temp = temp->next;
+        
+        }
+        return dummy->next;
+        
+    }
+};
